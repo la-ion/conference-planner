0f0b0aaf7e503b7bad337f5f3d52884800f23f1a
la-ion <ionelelaurentiu10@gmail.com>
2021-10-20 21:47:29 +0100

    Init project

--- src/main/java/com/laion/conference/planner/ConferencePlannerApplication.java
+++ src/main/java/com/laion/conference/planner/ConferencePlannerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConferencePlannerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConferencePlannerApplication.class, args);
+
 	}
 
 }
